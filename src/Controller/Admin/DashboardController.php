0fb0daa2eef648e4e4af77b558a626de0acb61c3
lamkimphu258 <lamkimphu258@gmail.com>
2021-11-14 09:51:54 +0700

    Feature: add authentication
    Add authentication
    
    Add EasyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+
+            MenuItem::section('Users'),
+            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+        ];
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        UserFactory::createOne([
+            'email' => 'admin@email.com',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
+        UserFactory::createOne([
+            'email' => 'volunteer@email.com',
+            'roles' => ['ROLE_VOLUNTEER']
+        ]);
+    }
+}
